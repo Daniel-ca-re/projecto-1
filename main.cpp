3ef54fbba958136b13252d92d84672b13ff73300
Daniel Cano <daniel.canor1@udea.edu.co>
2020-09-28 22:26:25 -0500

    1

--- main.cpp
+++ main.cpp
@@ -15,8 +15,25 @@ int main()
         if ( election ==  '1')
         {
             char letra;
-            cout << "selecciona una letra" << endl;
+            cout << "selecciona una letra\n: ";
             cin >> letra;
+            int num = int(letra);
+            if ((64<num && num<91)||(96<num && num<123))
+            {
+                if (num==65||num==69||num==73||num==79||num==85||
+                    num==97||num==101||num==105||num==111||num==117)
+                {
+                    cout <<letra<<" es una vocal"<< endl;
+                }
+                else
+                {
+                    cout <<letra<<" es una consonante"<< endl;
+                }
+            }
+            else
+            {
+                cout << "no es una letra\n";
+            }
         }
         else if  ( election=='2' )
         {
@@ -24,11 +41,11 @@ int main()
         }
         else if ((election=='x')||(election=='X'))
         {
-
+            break;
         }
         else
         {
-            cout <<'entrada invalida'<< endl;
+            cout <<"entrada invalida"<< endl;
         }
     }
 }
